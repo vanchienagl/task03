c308229bc408b7d58b17ed1c1a926a29d773adbc
TuDongNguyen <tu_dong@allgrow-labo.jp>
2022-02-14 16:13:37 +0700

    feat: update watch

--- plugins/watch.js
+++ plugins/watch.js
@@ -1,26 +1,34 @@
-import { posix } from "path";
+import { posix, relative, sep } from "path";
+import colors from "picocolors";
 import anymatch from "anymatch";
 
 function getShortName(file, root) {
   return file.startsWith(root + "/") ? posix.relative(root, file) : file;
 }
 
-function Watch(input = '') {
+function convertPath(path) {
+  return path.split(sep).join(posix.sep);
+}
+
+function Watch(paths = '', config = {}) {
   return {
     name: "watch",
     apply: 'serve',
 
-    handleHotUpdate({ file, server }) {
-      if (!input) return;
-      const { ws, config } = server;
-      const shortFile = getShortName(file, config.root);
-      if (anymatch(input, shortFile)) {
-        ws.send({
-          type: "full-reload",
-          path: "*",
-        });
-      }
-    },
+    config: () => ({ server: { watch: { disableGlobbing: false } } }),
+    configureServer({ watcher, ws, config: { logger, root } }) {
+      const { log = true, always = true, delay = 0 } = config;
+      const checkReload = (path) => {
+        const definitelyPosix = convertPath(path);
+        if (anymatch(paths, getShortName(definitelyPosix, root))) {
+          setTimeout(() => ws.send({ type: "full-reload", path: always ? "*" : path }), delay);
+          if (log)
+            logger.info(`${colors.green("page reload")} ${colors.dim(relative(root, path))}`, { clear: true, timestamp: true });
+        }
+      };
+      watcher.on("add", checkReload);
+      watcher.on("change", checkReload);
+    }
   };
 }
 
