c8df45ba32f33ac12049ef2d4c48075e5c467207
TuDongNguyen <tu_dong@allgrow-labo.jp>
2022-02-14 08:05:40 +0700

    fix: url

--- plugins/controller.js
+++ plugins/controller.js
@@ -2,13 +2,7 @@ const fs = require("fs");
 const path = require('path')
 var beautify_html = require('js-beautify').html;
 
-function Controller(options = {}) {
-  let config;
-  let default_options = {
-    styleDir: 'assets/css'
-  };
-  options = Object.assign(default_options, options);
-
+function Controller() {
   return {
     name: "controller",
     apply: "build",
@@ -25,18 +19,6 @@ function Controller(options = {}) {
       html = html.replace(
         /<script type="module" crossorigin src="\/_virtual\/modulepreload-polyfill.js"><\/script>/,
         ``
-      );
-      html = html.replace(
-        /<link rel="stylesheet" href="\/style.css">/,
-        `<link rel="stylesheet" href="\/${options.styleDir}\/style.css">`
-      );
-      html = html.replace(
-        /<script(.*)src="\/(.*)"/g,
-        `<script$1src="./$2"`
-      );
-      html = html.replace(
-        /<link(.*)href="\/(.*)"/g,
-        `<link$1href="./$2"`
       );
       html = beautify_html(html, { indent_size: 2, preserve_newlines: false, indent_inner_html: true });
       return html;
@@ -56,15 +38,6 @@ function Controller(options = {}) {
         }
       });
 
-      if (!fs.existsSync(path.join(config.build.outDir, options.styleDir))){
-        fs.mkdirSync(path.join(config.build.outDir, options.styleDir));
-      }
-      fs.rename(path.join(config.build.outDir, 'style.css'), path.join(config.build.outDir, options.styleDir, 'style.css'), (err) => {
-        config.logger.info(`Move style.css to ${options.styleDir}/style.css`);
-        if (err) {
-          console.log(err);
-        }
-      });
     },
   };
 }
